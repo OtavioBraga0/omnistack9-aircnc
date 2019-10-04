fa01cb73ff75f72a9c3b2198ea32b58c268e5c7a
Amex01 <amex01@MacBook-Pro.local>
2019-10-04 13:10:16 -0300

    Finalizado Projeto - Semana OmniStack 9.0

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const socketio = require("socket.io");
 
+const http = require("http");
 const path = require("path");
 
 const routes = require("./routes");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@omnistack-qrwcq.mongodb.net/omnistack09?retryWrites=true&w=majority",
@@ -22,9 +26,24 @@ mongoose.connect(
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar o corpo da requisição (para criação, edição)
 
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
---
+++ mobile/src/pages/List.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  AsyncStorage,
+  Alert,
+  StyleSheet
+} from "react-native";
+import socketio from "socket.io-client";
+
+import logo from "../assets/logo.png";
+import SpotList from "../components/SpotList";
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socketio("http://localhost:3333", {
+        query: { user_id }
+      });
+
+      socket.on("booking_response", booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? "APROVADA" : "REJEITADA"
+          }`
+        );
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(storageTechs => {
+      const techsArray = storageTechs.split(",").map(tech => tech.trim());
+
+      setTechs(techsArray);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 10
+  }
+});
